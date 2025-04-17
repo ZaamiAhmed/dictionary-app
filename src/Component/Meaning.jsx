97c9f325d7eabd3ff0878f652e563bab03748439
ZaamiAhmed <158285599+ZaamiAhmed@users.noreply.github.com>
2025-04-17 21:17:12 +0300

    added little css

--- src/Component/Meaning.jsx
+++ src/Component/Meaning.jsx
@@ -1,8 +1,8 @@
-
+import './meaning.css';
 export default function Meaning(props) {
     console.log(props.meanings);
     return(
-        <div>
+        <div className="meaning">
             <h3>{props.meanings.partOfSpeech}</h3>
           {props.meanings.definitions.map(function(definition,index) {
               return(
--- src/Component/Results.jsx
+++ src/Component/Results.jsx
@@ -1,9 +1,10 @@
 // import react from "react";
 import Meaning from "./Meaning";
+import './results.css';
 export default function Results(props) {
     // console.log(props.result);
     if (props.result) {   
-         return( <div>
+         return( <div className="results">
              <h2>{props.result.word}</h2>
              {props.result.meanings.map(function(meaning,index) {
               return   (<div key={index}>
