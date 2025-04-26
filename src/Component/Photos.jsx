c5c062778d6ff2507cca6d169f53c5238cd75a72
ZaamiAhmed <158285599+ZaamiAhmed@users.noreply.github.com>
2025-04-26 20:32:07 +0300

    fixed the alt img

--- src/Component/Photos.jsx
+++ src/Component/Photos.jsx
@@ -14,7 +14,7 @@ export default function Photos(props){
                <div className="col-sm-6 col-md-4" key={index}>
                    <a href={photo.src.original} target="_blank" rel="noreferrer">
                        
-               <img src={photo.src.landscape} className="img-fluid" />
+               <img src={photo.src.landscape} alt={photo.alt}  className="img-fluid" />
                
                </a>
                 </div>
